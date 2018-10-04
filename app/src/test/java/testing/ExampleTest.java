fc1af2395f24a2ae0b16884abc5c0926fdd8022f
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-10-04 17:48:28 -0300

    Updates, but Orchestrator isn't working yet...

--- app/src/test/java/testing/ExampleTest.java
+++ app/src/test/java/testing/ExampleTest.java
@@ -5,8 +5,8 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,8 +15,7 @@ public class ExampleTest {
 
     @Test
     public void useAppContext() {
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("net.rafaeltoledo.testing", appContext.getPackageName());
     }
 }
