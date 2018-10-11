ea2a67ee28366ca0a4e573cc8bdb9b36f8a66c90
Rafael Toledo <rafaeldtoledo@gmail.com>
2018-10-10 22:41:09 -0300

    Updates to beta02

--- app/src/test/java/testing/AnotherTest.java
+++ app/src/test/java/testing/AnotherTest.java
@@ -7,8 +7,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
