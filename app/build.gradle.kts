7105ebaf86c8f9586c3990ae62023a342e38f44b
Rafael Toledo <rafaeldtoledo@gmail.com>
2019-03-12 16:54:49 -0300

    Updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,16 +53,16 @@ dependencies {
     implementation("androidx.fragment:fragment:1.1.0-alpha04")
 
     implementation("androidx.constraintlayout:constraintlayout:2.0.0-alpha3")
-    implementation("com.google.android.material:material:1.1.0-alpha03")
+    implementation("com.google.android.material:material:1.1.0-alpha04")
 
-    bothTestImplementation("androidx.test:core-ktx:1.1.1-alpha01")
-    bothTestImplementation("androidx.test:runner:1.1.2-alpha01")
-    bothTestImplementation("androidx.test:rules:1.1.2-alpha01")
-    bothTestImplementation("androidx.test.espresso:espresso-core:3.1.2-alpha01")
-    bothTestImplementation("androidx.test.ext:junit-ktx:1.1.1-alpha01")
+    bothTestImplementation("androidx.test:core-ktx:1.1.1-alpha02")
+    bothTestImplementation("androidx.test:runner:1.1.2-alpha02")
+    bothTestImplementation("androidx.test:rules:1.1.2-alpha02")
+    bothTestImplementation("androidx.test.espresso:espresso-core:3.2.0-alpha02")
+    bothTestImplementation("androidx.test.ext:junit-ktx:1.1.1-alpha02")
     bothTestImplementation("androidx.fragment:fragment-testing:1.1.0-alpha04")
 
-    testImplementation("org.robolectric:robolectric:4.2-beta-1")
+    testImplementation("org.robolectric:robolectric:4.2")
 
-    androidTestUtil("androidx.test:orchestrator:1.1.2-alpha01")
+    androidTestUtil("androidx.test:orchestrator:1.1.2-alpha02")
 }
